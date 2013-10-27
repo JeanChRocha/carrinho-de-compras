9ae06820ed91699693fa34e3a50a3280e06a706c
JeanChRocha <jeanchrocha@gmail.com>
2013-10-27 21:08:34 -0200

    IEntrega.java

---
+++ test/br/calebe/exemplos/ex01/IEntrega.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calebe.exemplos.ex01;
+
+/**
+ *
+ * @author 31083331
+ */
+public interface IEntrega {
+    StatusEntrega getSituacaoEntrega(int pedido);
+}
